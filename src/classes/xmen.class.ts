935c6be9d203e5509cab8b99ceb45b8c37629220
dmarmijosa <daniel2667@hotmail.es>
2020-10-21 18:43:35 -0500

    Decoradores en typescript

--- src/classes/xmen.class.ts
+++ src/classes/xmen.class.ts
@@ -1,3 +1,8 @@
+function imprimirConsola (ConstructorClase:Function){
+    console.log(ConstructorClase);
+}
+@imprimirConsola
+//el @ es el decorador
 export class Xmen{
     constructor(
         public nombre:string,
